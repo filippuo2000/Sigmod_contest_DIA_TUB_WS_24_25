7f7e5b0c50c5546dc9f1c845d785477a5846df1e
fmatysik <01150849@pw.edu.pl>
2024-12-13 21:43:50 +0100

    Basic implementation - no optimization

---
+++ main.py
@@ -0,0 +1,81 @@
+import re
+import time
+
+from collections import defaultdict
+from src.app import StartQuery, EndQuery, MatchDocument, GenNextAvailableRes, get_queries
+
+
+def main():
+    with open("/Users/Filip/Downloads/tub_24/DIA/small_test.txt") as f:  
+    # with open("/Users/Filip/Downloads/testt.txt") as f:
+        num_cur_results: int = 0
+        cur_results = defaultdict(list)
+        while 1:
+            try:
+                line = next(f)
+                ch = re.search("^.", line).group()
+            
+                if ch == 's' and num_cur_results>0:
+                    # iterating over different documents and their corresponding results
+                    for id, res in cur_results.items():
+                        error_flag = False
+                        num_results, query_ids = GenNextAvailableRes(id)
+
+                        if num_results != len(res):
+                            error_flag = True
+                        
+                        if query_ids != res:
+                            error_flag = True
+                        
+                        if error_flag:
+                            raise ValueError(f"Results for document id {id} do not match,\n \
+                                                Num of true results: {len(res)}\n \
+                                                Num of predicted results is: {num_results}\n \
+                                                True results are: {res}\n \
+                                                Predicted results are: {query_ids}")
+                    num_cur_results = 0
+                    cur_results.clear()
+                        
+                if ch == 's':
+                    n = re.findall(r"\d{1,}", line)
+                    query_id, dist_type, tolerance, num_words = list(map(int, n))
+                    keywords = line.split()[-int(n[-1]):]
+                    assert keywords is not None
+                    StartQuery(query_id, dist_type, keywords, tolerance)
+                    # print("query id: ", query_id, "dist type: ", dist_type, "tolerance: ", tolerance, "num word in query: ", num_words)
+
+                if ch == 'm':
+                    # print(get_queries())
+                    n = re.findall(r"\d{1,}", line)
+                    doc_id, num_words = int(n[0]), int(n[1])
+                    words = line.split()[3:]
+                    MatchDocument(doc_id, words)
+                    # print("doc id: ", doc_id, "num of words: ", num_words)  
+
+                if ch == 'r':
+                    n = re.findall(r"\d{1,}", line)
+                    res_id, _, query_ids = int(n[0]), int(n[1]), n[2:]
+                    query_ids = list(map(int, query_ids))
+                    num_cur_results += 1
+                    cur_results[res_id] = sorted(query_ids)
+                    # match the document when all the results come
+                    # print("result id: ", res_id, "matched queries: ", query_id)
+
+                if ch == 'e':
+                    del_query_id = int(re.search(r"\d{1,}",line).group())
+                    EndQuery(del_query_id)
+                    # print("id to remove: ", int(del_query_id))
+
+                if ch is None:
+                    raise ValueError(f"char value {ch} is not expexted at the beginning of the line")
+
+            except StopIteration:
+                print("Reached end of file")
+                break
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    end = time.time()
+    print(f"Elapsed running time is: {abs(end-start)}")
---
+++ src/app.py
@@ -0,0 +1,118 @@
+import copy
+import Levenshtein
+
+from dataclasses import dataclass
+from enum import Enum
+from collections import defaultdict
+
+class DistanceType(Enum):
+    NORMAL = 0
+    HAMMING = 1
+    THIRD = 2
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}.{self.name}"
+
+@dataclass
+class Query:
+    id: int
+    dist_type: DistanceType # should be enum
+    keywords: list[str]
+    tolerance: int = None
+
+    def __repr__(self) -> str:
+        class_name = self.__class__.__name__
+        return f"Class {class_name} with attributes, query_id: {self.id}, dist_type: {self.dist_type}, keywords: {self.keywords}, tolerance: {self.tolerance}"
+
+@dataclass
+class Document:
+    id: int
+    num_results: int
+    query_ids: list[int]
+
+class DocumentCollection:
+    docs = defaultdict(Document)
+
+    @staticmethod 
+    def add_document(doc_id: int, document: Document):
+        DocumentCollection.docs[doc_id] = document
+    @staticmethod
+    def get_doc_results(doc_id: int):
+        return DocumentCollection.docs[doc_id]
+    @staticmethod
+    def remove_doc(doc_id: int):
+        if doc_id in DocumentCollection.docs:
+            del DocumentCollection.docs[doc_id]
+
+class QueryManager:
+    active_queries = defaultdict(Query)
+
+    @staticmethod
+    def add_query(query_id: int, query_object: Query):
+        QueryManager.active_queries[query_id] = query_object
+
+    @staticmethod
+    def remove_query(query_id):
+        if query_id in QueryManager.active_queries:
+            del QueryManager.active_queries[query_id]
+
+    @staticmethod
+    def get_active_queries():
+        return QueryManager.active_queries
+
+
+def calc_normal(query_word: str, doc_word: str, _):
+    return query_word==doc_word
+def calc_hamming(query_word: str, doc_word: str, tolerance: int = None):
+    if len(query_word) == len(doc_word):
+        return sum(c1!=c2 for c1, c2 in zip(query_word, doc_word)) <= tolerance      
+    else:
+        return False
+def calc_third(query_word: str, doc_word: str, tolerance: int = None):
+    return Levenshtein.distance(query_word, doc_word) <= tolerance
+
+
+def CalcMatch(match_type: DistanceType, query_words: list[str], doc_word: str, tolerance: int = None) -> bool:
+    if query_words:
+        calc_match = {
+            DistanceType.NORMAL: calc_normal,
+            DistanceType.HAMMING: calc_hamming,
+            DistanceType.THIRD: calc_third
+        }
+        words_to_remove = []
+        for query_word in query_words:
+            if calc_match[match_type](query_word, doc_word, tolerance):
+                words_to_remove.append(query_word)
+                # query_words.remove(query_word)
+        [query_words.remove(word) for word in words_to_remove]
+        
+    else:
+        raise ValueError("query_words list should not be empty when calculating distance")
+
+
+def get_queries():
+    return QueryManager.get_active_queries()
+def StartQuery(id: int, dist_type: int, words: list[str], tolerance: int = None) -> None:
+    if not words:
+        raise ValueError(f"empty word list for query id {id}, words are {words}")
+    QueryManager.add_query(id, Query(id, DistanceType(dist_type), words, tolerance))
+
+def EndQuery(id: int):
+    QueryManager.remove_query(id)
+
+def MatchDocument(id: int, doc_words: list[str]):
+    queries = copy.deepcopy(QueryManager.get_active_queries())
+    res_queries = []
+    for q_id, query in queries.items():
+        for word in doc_words:
+            CalcMatch(query.dist_type, query.keywords, word, query.tolerance)
+            if not query.keywords:
+                res_queries.append(q_id)
+                break
+    DocumentCollection.add_document(id, Document(id, len(res_queries), sorted(res_queries)))
+
+def GenNextAvailableRes(res_id: int):
+    results = copy.deepcopy(DocumentCollection.get_doc_results(res_id))
+    assert results.id == res_id
+    DocumentCollection.remove_doc(res_id)
+    return results.num_results, results.query_ids
